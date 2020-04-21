e96949640791e472958c1dd99ed69dbaa27cbd21
Nikita Shitov <56273378+shikitos@users.noreply.github.com>
2020-04-21 22:49:40 +0500

    Update JS
    Update js for work only with xs screens

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-var menu_icon = document.getElementById("menu_icon");
-var side_nav = document.getElementById("side_nav");
-var header = document.getElementById("header")
+function someFunc() {
+    // Выполняем действие, если ширина меньше 1000px
+    var w = window.innerWidth;
+    if (w < 1000) {
+        var menu_icon = document.getElementById("menu_icon");
+        var side_nav = document.getElementById("side_nav");
+        var header = document.getElementById("header")
 
-side_nav.style.right = "-200px";
-menu_icon.onclick = function() {
-    if (side_nav.style.right == "-200px") {
-        side_nav.style.right = "0";
-    } else {
         side_nav.style.right = "-200px";
+        menu_icon.onclick = function() {
+            if (side_nav.style.right == "-200px") {
+                side_nav.style.right = "0";
+            } else {
+                side_nav.style.right = "-200px";
+            }
+        };
     }
-};
-/*
-document.onclick = function() {
-    if (side_nav.style.right == "0") {
-        side_nav.style.right = "-200px";
-    } else {
-        side_nav.style.right = "0";
-    }
-}; */
+}
+
+// Выполняем заново при изменении размера окна
+window.addEventListener('resize', function() {
+    console.log("Размер окна изменен");
+    someFunc();
+});
